fe665e6823ec850fca6a6ccdb6999ba61514f991
Sebastian Perez Betancur <Sebbepebe27@gmail.com>
2024-06-29 17:07:18 +0000

    we create a while with the variables letters , list, and guessed letters

--- run.py
+++ run.py
@@ -45,3 +45,40 @@ def hangman():
 
 
 
+    # Get the answers from the user, create a while loop with the number of lives
+    # and the pending letters to be discovered
+
+    while len(letters_to_guess) > 0 and lives > 0:
+        
+        print(f"You have {lives} lives left and you have used these letters: {' '.join(guessed_letters)}")
+
+        # Show the actual status of the game with a list comprehension 
+
+        word_list = [letter if letter in guessed_letters else '-' for letter in word]
+        print(lifes_visual_dictionary[lives])
+        print(f"Word: {' '.join(word_list)}")
+
+        # The user chooses a new letter
+        user_letter = input('Choose one letter: ').upper()
+
+        # If the letter chosen by the user is in the alphabet
+        # and is not in the set of letters that have already been entered,
+        # the letter is added to the set of entered letters.
+        if user_letter in alphabet - guessed_letters:
+            guessed_letters.add(user_letter)
+
+            # If the letter is in the word, remove the letter 
+            # from the set of letters pending to be guessed
+            if user_letter in letters_to_guess:
+                letters_to_guess.remove(user_letter)
+                print('')
+            else:
+                # If the letter is not in the word, take a life.
+                lives -= 1
+                print(f"\nYour letter, {user_letter}, is not in the word.")
+        elif user_letter in guessed_letters:
+            # If the letter chosen by the user has already been entered.
+            print("\nYou already chose that letter. Please choose a new letter.")
+        else:
+            # If the letter is not valid.
+            print("\nThis letter is not valid.")
